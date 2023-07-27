97d65f4af817ffcbd509281181c946e04a6a2036
Loveleen Kaur <126374845+leenubhagtana@users.noreply.github.com>
2023-07-27 19:30:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; 
 import Login from './Login';
 import Users from './Userslist'; 
 import Movies from './Movies'; 
